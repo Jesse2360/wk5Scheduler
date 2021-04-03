afdbe60767e34bb0590a4446b8f70c15c517ccdf
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-04-03 00:28:01 -0500

    added function for row 9am save local storage

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
+
+/* When I click a time block I can enter a new event and once i click the save button
+my new event will save even if refresh the page. */
+function nineOClock() {
+    var textBox = document.getElementById("event9");
+    var outTextBox = document.getElementById("event9");
+    var saveBtn = document.getElementById("button9");
+  
+    saveBtn.addEventListener("click", newEvent);
+  
+    outTextBox.innerHTML = localStorage.getItem("content");
+    textBox.value = localStorage.getItem("content");
+  
+    function newEvent() {
+      localStorage.setItem("content", textBox.value);
+  
+      outTextBox.innerHTML = textBox.value;
+    }
+  }
+  // calling the function
+nineOClock()
+
+
+
+
+
+
+
+
+
+
